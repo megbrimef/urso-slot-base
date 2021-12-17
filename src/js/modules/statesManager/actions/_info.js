02405875d4d80b58ef1dda988fe2e8cd62683914
alkott <alkott@hotmail.com>
2021-12-17 13:26:08 +0200

    update

--- src/js/modules/statesManager/actions/_info.js
+++ src/js/modules/statesManager/actions/_info.js
@@ -1,10 +1,9 @@
 Urso.SlotBase.Modules.StatesManager.Actions = {
-    TransportInitAction: require('./transportInitAction'),
-    ServerApiVersionRequestAction: require('./serverApiVersionRequestAction'),
-    ServerCheckBrokenGameRequestAction: require('./serverCheckBrokenGameRequestAction'),
-    ServerAuthRequestAction: require('./serverAuthRequestAction'),
     ServerSpinRequestAction: require('./serverSpinRequestAction'),
     ServerBalanceRequestAction: require('./serverBalanceRequestAction'),
     WaitingForInteractionAction: require('./waitingForInteractionAction'),
-    UpdateServerSettingsAction: require('./updateServerSettingsAction')
+    UpdateServerSettingsAction: require('./updateServerSettingsAction'),
+    ServerCheckBrokenGameRequestAction: require('./serverCheckBrokenGameRequestAction'),
+    BaseTransportAction: require('./baseTransportAction'),
+    ServerAuthRequestAction: require('./serverAuthRequestAction'),
 };
