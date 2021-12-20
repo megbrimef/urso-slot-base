0446a08b9df52461d7f45f651457200e19e9b264
alkott <alkott@hotmail.com>
2021-12-20 21:27:04 +0200

    transport update

--- src/js/modules/statesManager/actions/_info.js
+++ src/js/modules/statesManager/actions/_info.js
@@ -1,4 +1,5 @@
 Urso.SlotBase.Modules.StatesManager.Actions = {
+    TransportInitAction: require('./transportInitAction'),
     ServerSpinRequestAction: require('./serverSpinRequestAction'),
     ServerBalanceRequestAction: require('./serverBalanceRequestAction'),
     WaitingForInteractionAction: require('./waitingForInteractionAction'),
