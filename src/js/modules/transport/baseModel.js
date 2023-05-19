0f68b586f86824a36c18b954ba590f3bdcd4a04f
megbrimef <megbrimef@gmail.com>
2023-05-19 15:13:50 +0300

    0.7.29

--- src/js/modules/transport/baseModel.js
+++ src/js/modules/transport/baseModel.js
@@ -1,10 +1,12 @@
 class BaseModel {
+
     constructor(action, sesId, data = {}) {
         this.action = action;
         this.result = true;
         this.sesId = sesId;
         this.data = data;
     }
+
 }
 
 module.exports = BaseModel;
